4f83bbcc240cc646d5e64b312f27d5869250febb
Iaritzza <iaritzzayara@gmail.com>
2021-08-30 23:13:19 -0300

    Projeto criado e adicionado bundleconfig

---
+++ Projeto1_Curso2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Projeto1_Curso2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
